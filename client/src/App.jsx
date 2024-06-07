8f88ebef6b02ae95d806fea9353d0996e3b0c20a
unknown <sornram.spy@gmail.com>
2024-06-07 15:16:43 +0700

    feat:React

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,54 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [blogProduct, setBlogProduct] = useState([]);
+
+  const getBlogProduct = async () => {
+    const getProduct = await axios.get("http://localhost:4001/products");
+    setBlogProduct(getProduct.data.data);
+  };
+
+  const deleteBlogProduct = async (id) => {
+    await axios.delete(`http://localhost:4001/products/${id}`);
+    setBlogProduct((prevProducts) => prevProducts.filter(product => product.id !== id));
+  };
+
+  useEffect(() => {
+    getBlogProduct();
+  }, []);
+
   return (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
       <div className="product-list">
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
-          </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
-
-          <button className="delete-button">x</button>
-        </div>
+        {blogProduct.map((product) => {
+          return (
+            <div className="product" key={product.id}>
+              <div className="product-preview">
+                <img
+                  src={product.image}
+                  alt="some product"
+                  width="350"
+                  height="350"
+                />
+              </div>
+              <div className="product-detail">
+                <h1>Product name: {product.name}</h1>
+                <h2>Product price: {product.price}</h2>
+                <p>Product description: {product.description}</p>
+              </div>
+              <button className="delete-button" onClick={() => deleteBlogProduct(product.id)}>x</button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 }
 
 export default App;
+
